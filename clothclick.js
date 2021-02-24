583a6fa43c01eb53df062c99a09fbd16443dd2e1
frankli-n <frankiehaubrich@gmail.com>
2021-02-24 14:27:27 -0800

    a

---
+++ clothclick.js
@@ -0,0 +1,7 @@
+// // 
+// // For clicking on the doom button
+// // 
+// document.querySelector('.bush-basha').addEventListener('click', () => {
+//   document.getElementById("yonder").style.visibility = "hidden";
+//   document
+// })
