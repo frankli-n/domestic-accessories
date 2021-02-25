08609e8e72bff3c36449385bd66936a9e2a28003
frankli-n <frankiehaubrich@gmail.com>
2021-02-24 17:01:00 -0800

    a

--- clothclick.js
+++ clothclick.js
@@ -5,3 +5,13 @@
 //   document.getElementById("yonder").style.visibility = "hidden";
 //   document
 // })
+
+function changeImage(){
+	console.log(document.getElementById("a").src)
+	if (document.getElementById("a").src == "http://localhost:3002/pics/gymshark.png") {
+		document.getElementById("a").src = "http://localhost:3002/pics/gymshark-back.png"
+	}
+	else {
+		document.getElementById("a").src = "http://localhost:3002/pics/gymshark.png"
+	}
+}
